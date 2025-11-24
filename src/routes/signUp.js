e554280e733e080637eca56c9b2f5c8c26e8a6f3
Ferhad-Karimli <ferhad.karimli.077@gmail.com>
2025-11-24 19:01:04 +0400

    Updates

--- src/routes/signUp.js
+++ src/routes/signUp.js
@@ -2,6 +2,7 @@ const { signUp } = require('../controllers/signUp/signUp');
 const express = require('express');
 
 const router = express.Router();
+
 router.post('/api/register', signUp);
 
 module.exports = router;
