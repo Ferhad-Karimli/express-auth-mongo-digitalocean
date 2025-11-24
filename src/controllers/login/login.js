15e4af78297fb609e7e5e05faf1e7063634a844a
Ferhad-Karimli <ferhad.karimli.077@gmail.com>
2025-11-24 21:10:07 +0400

    userlist Added

--- src/controllers/login/login.js
+++ src/controllers/login/login.js
@@ -37,3 +37,15 @@ exports.login = async (req, res) => {
     res.status(500).json({ message: 'Something went wrong' });
   }
 };
+
+exports.userList = async (req,res) => {
+  try{
+    const users = await  SignUpSchema.find({})
+    console.log(users)
+    res.status(200).json({users})
+
+  }
+  catch(error){
+    res.status(500).json({ message: 'Something went wrong' });
+  }
+}
--- src/routes/login.js
+++ src/routes/login.js
@@ -1,7 +1,8 @@
-const { login } = require('../controllers/login/login');
+const { login , userList} = require('../controllers/login/login'); 
 const express = require('express');
 
 const router = express.Router();
 
 router.post('/api/login', login);
+router.get('/api/users', userList);
 module.exports = router;
