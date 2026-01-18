4d989ee4628a111a0e534a4755599dc4c571f489
Ferhad-Karimli <ferhad.karimli.077@gmail.com>
2026-01-18 22:00:47 +0400

    CI CD 10

--- src/routes/signUp.js
+++ src/routes/signUp.js
@@ -2,7 +2,6 @@ const { signUp } = require('../controllers/signUp/signUp');
 const express = require('express');
 
 const router = express.Router();
-
 router.post('/api/register', signUp);
 
 module.exports = router;
